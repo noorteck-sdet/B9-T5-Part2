79241006f6eeec3be2a114dfb63db6caf2618a08
zaki <zaki@localhost>
2023-04-11 15:27:45 -0700

    zaki's project hw

---
+++ src/zaki_hw/HogRider.java
@@ -0,0 +1,24 @@
+package zaki_hw;
+
+public class HogRider {
+	public static void main(String[] args) {
+		
+		HogRider.firstMethod();
+		HogRider.secondMethod();
+	}
+	
+	
+	
+	public static void firstMethod () {
+		System.out.println("First Method");
+	}
+	public static void secondMethod () {
+		System.out.println("Second Method");
+	}
+	
+	
+	
+	
+	
+	
+}
