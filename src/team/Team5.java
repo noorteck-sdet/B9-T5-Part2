88229965b3c4d77c50164994d701a0728af2952e
zaki <zaki@localhost>
2023-04-11 15:35:28 -0700

    zaki"s edit

--- src/team/Team5.java
+++ src/team/Team5.java
@@ -4,21 +4,33 @@ public class Team5 {
 
 	public static void main(String[] args) {
 
+		Team5.isValid(13);
 	}
 
 	public void getInfo(String first_Name) {
-		
-		// add your name 
-		
-	    first_Name = "Abdessalam";
-		
+
+		// add your name
+
+		first_Name = "Abdessalam";
 
 	}
-	public void getMoreInfo (String state) {
-		
+
+	public void getMoreInfo(String state) {
+
 		state = "WA";
-		
-		
+
+	}
+
+	public static boolean isValid(int age) {
+		boolean result = false;
+
+		if (age > 18) {
+			result = true;
+			System.out.println(result);
+		}
+System.out.println("Zaki's edith " );
+		return result;
+
 	}
 
 }
