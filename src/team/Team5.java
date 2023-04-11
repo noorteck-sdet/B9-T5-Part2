055998ff76ffd7eee6fc30f52ea6ad4ea080dc6b
Abdessalam <Abdessalam@192.168.148.117>
2023-04-11 13:22:20 +0000

    Second Commit

---
+++ src/team/Team5.java
@@ -0,0 +1,24 @@
+package team;
+
+public class Team5 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public void getInfo(String first_Name) {
+		
+		// add your name 
+		
+	    first_Name = "Abdessalam";
+		
+
+	}
+	public void getMoreInfo (String state) {
+		
+		state = "WA";
+		
+		
+	}
+
+}
