11aafb55897c7512a22a72c99a1b4751425a8f66
Abdessalam <Abdessalam@192.168.148.117>
2023-04-12 06:12:03 +0000

    Second Commit

--- src/page/LoginPage.java
+++ src/page/LoginPage.java
@@ -1,7 +1,25 @@
 package page;
 
+import java.util.Arrays;
+
 public class LoginPage {
 	public static void main(String[] args) {
 		System.out.println("part 2 of fahim hw , random edit on someone eles's code ");
+	
+	  String str =  "ewaxajxysgjlakpqsqosiqnnzjakajxaox";
+	  
+	   char[] strArr = str.toCharArray();
+	   
+	   Arrays.sort(strArr);
+	   
+	    for (char values: strArr) {
+	    	
+	    	System.out.println(values);
+	    	
+	    }
+	    
+	   
 	}
+	
+	
 }
